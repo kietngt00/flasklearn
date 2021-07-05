5c9df8b709ee6bc916ddd028996d4603613a0d49
KietNgT <kietngt00@gmail.com>
2021-07-05 22:06:14 +0700

    Using MySQL

--- app.py
+++ app.py
@@ -1,63 +1,60 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask.wrappers import Request
-from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
+import yaml
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    finish = db.Column(db.String(3), default='No')
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
 
-    def __repr__(self):
-        return '<Task %r>' % self.id
+mysql = MySQL(app)
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == "POST":
         task_content = request.form['content']
-        new_task = Todo(content=task_content, finish='No')
-
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "There was an issue adding your task"
-
+        task_date = str(datetime.utcnow())
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO Todo(content, date_created) VALUES(%s,%s)",(task_content, task_date))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/')
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
+        cur = mysql.connection.cursor()
+        tasks = cur.execute("SELECT * FROM Todo ORDER BY date_created")
+        tasks = cur.fetchall()
+        cur.close()
         return render_template('index.html', tasks = tasks)
+    
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting task'
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM Todo WHERE id=%d"%id)
+    mysql.connection.commit()
+    cur.close()
+    return redirect('/')
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    task = Todo.query.get_or_404(id)
-
+    cur = mysql.connection.cursor()
     if request.method == "POST":
-        task.content = request.form['content']
-        task.finish = request.form['finish']
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
+        content = request.form['content']
+        finish = request.form['finish']
+        cur.execute("UPDATE Todo SET content=%s, finish=%s WHERE id=%s",(content, finish, id))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/')
     else:
-        return render_template('update.html', task=task)
+        task = cur.execute("SELECT * FROM Todo WHERE id=%d"%id)
+        task = cur.fetchall()
+        cur.close()
+        return render_template('update.html', task=task[0])
 
 if __name__ == "__main__":
     app.run(debug=True)
